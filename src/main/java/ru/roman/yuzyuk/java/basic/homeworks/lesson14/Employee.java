8e50f51e961eb727df504967f5363aad1fc8679c
Roman Yuzyuk <romanyuzyukfront@gmail.com>
2024-08-22 14:37:06 +0300

    Lesson 14

---
+++ src/main/java/ru/roman/yuzyuk/java/basic/homeworks/lesson14/Employee.java
@@ -0,0 +1,37 @@
+package ru.roman.yuzyuk.java.basic.homeworks.lesson14;
+
+public class Employee {
+    private String name;
+    private byte age;
+
+    public Employee() {}
+
+    public Employee(String name, byte age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
