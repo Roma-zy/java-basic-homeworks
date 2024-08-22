a39e83ba1d607991e6e92d4c5c22780535d0a3b7
Roman Yuzyuk <romanyuzyukfront@gmail.com>
2024-08-22 16:15:46 +0300

    Lesson 14: change ArrayList to List

--- src/main/java/ru/roman/yuzyuk/java/basic/homeworks/lesson14/Main.java
+++ src/main/java/ru/roman/yuzyuk/java/basic/homeworks/lesson14/Main.java
@@ -1,13 +1,14 @@
 package ru.roman.yuzyuk.java.basic.homeworks.lesson14;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getArrayListIntegers(1, 9999));
+        System.out.println(getListIntegers(1, 9999));
 
-        ArrayList<Integer> integers = new ArrayList<>();
+        List<Integer> integers = new ArrayList<>();
         integers.add(1);
         integers.add(2);
         integers.add(1);
@@ -22,24 +23,24 @@ public class Main {
         System.out.println(integers);
 
 
-        ArrayList<Employee> employeeArrayList = new ArrayList<>();
-        employeeArrayList.add(new Employee("Вася", (byte) 22));
-        employeeArrayList.add(new Employee("Петя", (byte) 60));
-        employeeArrayList.add(new Employee("Ира", (byte) 25));
-        employeeArrayList.add(new Employee("Ян", (byte) 74));
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee("Вася", (byte) 22));
+        employeeList.add(new Employee("Петя", (byte) 60));
+        employeeList.add(new Employee("Ира", (byte) 25));
+        employeeList.add(new Employee("Ян", (byte) 74));
 
-        System.out.println(getEmployeeNames(employeeArrayList));
-        System.out.println(filterEmployeeByAge(employeeArrayList, (byte) 60));
+        System.out.println(getEmployeeNames(employeeList));
+        System.out.println(filterEmployeeByAge(employeeList, (byte) 60));
 
-        System.out.println(averageAgeEmployeesExceedsSpecifiedValue(employeeArrayList, (byte) 30));
-        System.out.println(averageAgeEmployeesExceedsSpecifiedValue(employeeArrayList, (byte) 50));
+        System.out.println(averageAgeEmployeesExceedsSpecifiedValue(employeeList, (byte) 30));
+        System.out.println(averageAgeEmployeesExceedsSpecifiedValue(employeeList, (byte) 50));
 
-        System.out.println(getYoungestEmployee(employeeArrayList));
+        System.out.println(getYoungestEmployee(employeeList));
     }
 
 
-    public static ArrayList<Integer> getArrayListIntegers(int min, int max) {
-        ArrayList<Integer> result = new ArrayList<>(max - min);
+    public static List<Integer> getListIntegers(int min, int max) {
+        List<Integer> result = new ArrayList<>(max - min);
 
         for (int i = min; i <= max; i++) {
             result.add(i);
@@ -48,31 +49,31 @@ public class Main {
         return result;
     }
 
-    public static int getSumItemsMoreThenFive(ArrayList<Integer> list) {
+    public static int getSumItemsMoreThenFive(List<Integer> list) {
         return !list.isEmpty() ? list.stream().reduce((acc, item) -> acc + item).get() : 0;
     }
 
-    public static void changeAllItems(int value, ArrayList<Integer> list) {
+    public static void changeAllItems(int value, List<Integer> list) {
         for (int i = 0; i < list.size(); i++) {
             list.set(i, value);
         }
     }
 
-    public static void addValueEachElement(int value, ArrayList<Integer> list) {
+    public static void addValueEachElement(int value, List<Integer> list) {
         for (int i = 0; i < list.size(); i++) {
             list.set(i, list.get(i) + value);
         }
     }
 
-    public static ArrayList<String> getEmployeeNames (ArrayList<Employee> employees) {
-        return (ArrayList<String>) employees.stream().map(employee -> employee.getName()).collect(Collectors.toList());
+    public static List<String> getEmployeeNames (List<Employee> employees) {
+        return employees.stream().map(employee -> employee.getName()).collect(Collectors.toList());
     }
 
-    public static ArrayList<Employee> filterEmployeeByAge (ArrayList<Employee> employees, byte minAge) {
-        return (ArrayList<Employee>) employees.stream().filter(employee -> employee.getAge() >= minAge).collect(Collectors.toList());
+    public static List<Employee> filterEmployeeByAge (List<Employee> employees, byte minAge) {
+        return employees.stream().filter(employee -> employee.getAge() >= minAge).collect(Collectors.toList());
     }
 
-    public static boolean averageAgeEmployeesExceedsSpecifiedValue (ArrayList<Employee> employees, byte age) {
+    public static boolean averageAgeEmployeesExceedsSpecifiedValue (List<Employee> employees, byte age) {
         if (employees.isEmpty()) {
             return false;
         }
@@ -82,7 +83,7 @@ public class Main {
         return averageAge > age;
     }
 
-    public static Employee getYoungestEmployee (ArrayList<Employee> employees) {
+    public static Employee getYoungestEmployee (List<Employee> employees) {
         return employees
                 .stream()
                 .reduce(new Employee(), (acc, item) -> acc.getAge() < item.getAge() ? acc : item );
