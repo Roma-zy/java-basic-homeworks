a0d291682b4d07a46f01eb23059e2b0f76cd8aba
Roman Yuzyuk <romanyuzyukfront@gmail.com>
2024-08-26 18:19:52 +0300

    Lesson 16: fix Node class

--- src/main/java/ru/roman/yuzyuk/java/basic/homeworks/lesson16/Node.java
+++ src/main/java/ru/roman/yuzyuk/java/basic/homeworks/lesson16/Node.java
@@ -1,34 +1,16 @@
 package ru.roman.yuzyuk.java.basic.homeworks.lesson16;
 
 public class Node<T> {
-    private T value;
+    private final T value;
     private Node<T> leftSide;
     private Node<T> rightSide;
-
-    public Node() {
-    }
 
     public Node(T value) {
         this.value = value;
     }
 
-    public Node(T value, Node<T> leftSide) {
-        this.value = value;
-        this.leftSide = leftSide;
-    }
-
-    public Node(T value, Node<T> leftSide, Node<T> rightSide) {
-        this.value = value;
-        this.leftSide = leftSide;
-        this.rightSide = rightSide;
-    }
-
     public T getValue() {
         return value;
-    }
-
-    public void setValue(T value) {
-        this.value = value;
     }
 
     public Node<T> getLeftSide() {
